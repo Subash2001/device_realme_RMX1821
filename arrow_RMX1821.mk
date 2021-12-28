eb4fd430722b2d1710c73eeeacb7b826194bb5d0
kubersharma001 <kubersharma001@gmail.com>
2021-12-28 17:07:15 +0530

    RMX1821: Include Lawnchair when available
    https://github.com/ArrowOS/android_vendor_lawnchair
    
    Change-Id: I7ba9c1aa4d9e1d661f026cac3ababdebd89de8ae

--- arrow_RMX1821.mk
+++ arrow_RMX1821.mk
@@ -21,6 +21,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit common ArrowOS configurations
 $(call inherit-product, vendor/arrow/config/common.mk)
 
+# ArrowOS additions
+$(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
+
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 720
 
