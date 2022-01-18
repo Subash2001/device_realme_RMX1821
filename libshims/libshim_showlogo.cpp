39ae178aff3f18029f6a09ada5f1e93f91413a09
SamarV-121 <samarvispute121@pm.me>
2022-01-18 11:05:38 +0000

    RMX1821: libshims: Update showlogo shim to S
    Change-Id: I53574d0200efc8d0cf58468c38ac5a56aff1573c
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>
    Signed-off-by: NaissAA <nasifahmed150@gmail.com>
    Signed-off-by: subash2001 <subashselvam0@gmail.com>

--- libshims/libshim_showlogo.cpp
+++ libshims/libshim_showlogo.cpp
@@ -7,10 +7,10 @@ extern "C" void _ZN7android21SurfaceComposerClient11Transaction20setDisplayProje
   t->setDisplayProjection(token, static_cast<android::ui::Rotation>(orientation), layerStackRect, displayRect);
 	}
 
-extern "C" void _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlENS_13LayerMetadataEPj(const String8& name, uint32_t w, uint32_t h, PixelFormat format, uint32_t flags, SurfaceControl* parent, LayerMetadata metadata, uint32_t* outTransformHint);
+extern "C" void _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijRKNS_2spINS_7IBinderEEENS_13LayerMetadataEPj(const String8& name, uint32_t w, uint32_t h, PixelFormat format, uint32_t flags, SurfaceControl* parent, LayerMetadata metadata, uint32_t* outTransformHint);
 
 extern "C" void _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlENS_13LayerMetadataE(const String8& name, uint32_t w, uint32_t h, PixelFormat format, uint32_t flags, SurfaceControl* parent, LayerMetadata metadata) {
-	_ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlENS_13LayerMetadataEPj(name, w, h, format, flags, nullptr, metadata, nullptr);
+		_ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijRKNS_2spINS_7IBinderEEENS_13LayerMetadataEPj(name, w, h, format, flags, nullptr, metadata, nullptr);
 		}
 
 extern "C" void _ZN7android14SurfaceControl7releaseEv() { }
