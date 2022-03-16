d5aaf430a743695644831d32d510391dc2393b09
subash2001 <subashselvam0@gmail.com>
2022-03-16 15:23:04 +0000

    RMX1821: Aosp Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,9 +17,9 @@
 DEVICE_PATH := device/realme/RMX1821
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_RMX1821.mk 
+    $(LOCAL_DIR)/aosp_RMX1821.mk 
 
 COMMON_LUNCH_CHOICES := \
-    nad_RMX1821-user \
-    nad_RMX1821-userdebug \
-    nad_RMX1821-eng
+    aosp_RMX1821-user \
+    aosp_RMX1821-userdebug \
+    aosp_RMX1821-eng
--- aosp_RMX1821.mk
+++ aosp_RMX1821.mk
@@ -18,18 +18,16 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+# Inherit some common Aosp stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-# Gapps
-TARGET_BOOT_ANIMATION_RES:= 720
-TARGET_GAPPS_ARCH := arm64
-IS_PHONE := true
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 720
 
 # Inherit from Realme RMX1821
 $(call inherit-product, device/realme/RMX1821/device.mk)
 
-PRODUCT_NAME := nad_RMX1821
+PRODUCT_NAME := aosp_RMX1821
 PRODUCT_DEVICE := RMX1821
 PRODUCT_MANUFACTURER := Realme
 PRODUCT_BRAND := Realme
