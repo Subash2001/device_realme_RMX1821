762f3f9081a0dc8c370a570439deb79753d8149e
SubAsH <subash662009@gmail.com>
2022-05-14 15:39:24 +0000

    RMX1821: AEX bring up

--- aosp_RMX1821.mk
+++ aosp_RMX1821.mk
@@ -19,11 +19,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Aosp stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 720
-
 TARGET_SUPPORTS_QUICK_TAP := true
 
 # Inherit from Realme RMX1821
