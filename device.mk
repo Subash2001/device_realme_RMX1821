4445a01fcd6587688c84900dcbb27cb6c1a68e1e
Albert I <kras@raphielgang.org>
2022-09-18 12:11:23 +0530

    RMX1821: Inherit several Android Go configurations
    Beneficial for everyone, even on non-Go devices.
    
    Change-Id: I088fd00500ce10df5f413cb762aaed2ec00f5dfc

--- device.mk
+++ device.mk
@@ -109,6 +109,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.face.moto_unlock_service=true
 
+# Inherit several Android Go Configurations (Beneficial for everyone, even on non-Go devices)
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # IMS
 $(call inherit-product, vendor/mediatek-ims/ims-vendor.mk)
 
