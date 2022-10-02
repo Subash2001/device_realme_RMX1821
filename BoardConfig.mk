d470e950c1ca93ca35a9b304a23c5634cbf14a4d
Alexander Koskovich <akoskovich@pm.me>
2022-10-02 21:38:39 +0530

    RMX1821: Move BT profiles props to vendor
     * Makes it GSI compatible
    
    Change-Id: I8f7caf241d508af59c16482e6185c42f23f8a460

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,7 +103,7 @@ TARGET_COPY_OUT_PRODUCT := system/product
 # System Properties
 TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
-TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Use mke2fs to create ext4 images
 TARGET_USES_MKE2FS := true
